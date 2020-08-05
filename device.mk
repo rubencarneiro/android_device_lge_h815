03441636128c088844226ca234b4546b7cecd2a8
Ari Börde Kröyer <ari.kroyer@online.no>
2020-08-05 20:34:16 +0200

    Inject overlay files for rsyslog fix and for sound

--- device.mk
+++ device.mk
@@ -42,7 +42,9 @@ $(call inherit-product, device/lge/g4-common/g4.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ubuntu/android.conf:system/halium/etc/ubuntu-touch-session.d/android.conf \
     $(LOCAL_PATH)/ubuntu/70-android.rules:system/halium/lib/udev/rules.d/70-android.rules \
-#    $(LOCAL_PATH)/ubuntu/init_setup_first_boot.sh:system/halium/var/lib/lxc/android/pre-start.d/init_setup_first_boot.sh \
+    $(LOCAL_PATH)/ubuntu/rsyslog.conf:etc/rsyslog.conf \
+    $(LOCAL_PATH)/ubuntu/touch.pa:etc/pulse/touch.pa \
+
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.vanilla.abi=1
