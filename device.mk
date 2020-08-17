87cf85c8b0b1308649d3771990ddef6bc56ea554
Ari Börde Kröyer <ari.kroyer@online.no>
2020-08-17 22:45:13 +0200

    Removed direct_ubuntu_application hybris-test packages

--- device.mk
+++ device.mk
@@ -30,9 +30,6 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # UBPorts
  PRODUCT_PACKAGES += \
      libubuntu_application_api \
-#     direct_ubuntu_application_sensors_c_api_for_hybris_test \
-#     direct_ubuntu_application_sensors_for_hybris_test \
-#     direct_ubuntu_application_gps_c_api_for_hybris_test \
      libcameraservice \
      libdroidmedia \
      libcamera_compat_layer \
@@ -69,7 +66,7 @@ MINIMEDIA_SENSORSERVER_DISABLE := 1
 
 # telepathy-ofono quirks
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.t-o.quirk.forcesink=sink.primary_output  \
+    ro.t-o.quirk.forcesink=sink.primary_output \
     ro.t-o.quirk.forcesource=source.primary_input \
     ril.device=ril
 
